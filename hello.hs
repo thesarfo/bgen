73b813f7b398f7ccf84dba544ed89fb705e1bc35
Ernest Sarfo <ernestsarfo100@gmail.com>
2025-02-15 12:02:31 +0000

    add types for all functions and change html function implementations to use el

--- hello.hs
+++ hello.hs
@@ -1,13 +1,34 @@
+main :: IO ()
 main = putStrLn myhtml
 
-myhtml = makeHtml "Hello title" "Hello, world!"
+myhtml :: String
+myhtml =
+  makeHtml
+    "Hello title"
+    (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
 
+
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
-html_ content = "<html>" <> content <> "</html>"
-   
-body_ content = "<body>" <> content <> "</body>"
+html_ :: String -> String
+html_ = el "html"
 
-head_ content = "<head>" <> content <> "</head>"
+body_ :: String -> String
+body_ = el "body"
 
-title_ content = "<title>" <> content <> "</title>"
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
